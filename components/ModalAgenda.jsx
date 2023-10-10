7a171dd5e3820116317fb6efef15faa4c2e4c9c3
Alvaro pimentel <alvaroaugusto@alvaropimentel.eti.br>
2023-10-10 12:02:59 -0300

    atualizações agenda

--- components/ModalAgenda.jsx
+++ components/ModalAgenda.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Modal, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Modal, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default function ModalAgenda({ isModalVisible, setModalVisible, addEvent }){
-    const [newEventText, setNewEventText] = useState('');
+    const [ tituloCompromisso, setTituloCompromisso ] = useState('');
+    const [ descricaoCompromisso, setDescricaoCompromisso ] = useState('');
 
     return(
         <>
@@ -16,21 +17,45 @@ export default function ModalAgenda({ isModalVisible, setModalVisible, addEvent 
         >
             <View style={styles.centeredView}>
                 <View style={styles.modalView}>
-                    <Text style={{ marginBottom: 10 }}>Novo Compromisso</Text>
+                    <Text style={{ marginBottom: 10, color: '#fff' }}>Novo Compromisso</Text>
                     <TextInput
                         style={styles.input}
-                        value={newEventText}
-                        onChangeText={setNewEventText}
-                        placeholder="Digite o compromisso"
+                        value={tituloCompromisso}
+                        onChangeText={setTituloCompromisso}
+                        placeholder="Digite o titulo"
+                        placeholderTextColor={'#fff'}
                     />
-                    <Button title="Adicionar" onPress={() => {
-                        addEvent(newEventText);
-                        setNewEventText('');
-                        setModalVisible(false);
-                    }} />
-                    <Button title="Cancelar" onPress={() => {
-                        setModalVisible(false);
-                    }} />
+                    
+                    <TextInput
+                        style={styles.input}
+                        value={descricaoCompromisso}
+                        onChangeText={setDescricaoCompromisso}
+                        placeholder="Digite a descrição"
+                        placeholderTextColor={'#fff'}
+                    />
+        
+                    <View style={{ flexDirection: 'row', marginTop: 5 }}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => {
+                                addEvent(tituloCompromisso, descricaoCompromisso);
+                                setTituloCompromisso('');
+                                setDescricaoCompromisso('');
+                                setModalVisible(false);
+                            }}
+                        >
+                            <Text style={styles.buttonText}>Adicionar</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => {
+                                setModalVisible(false);
+                            }}
+                        >
+                            <Text style={styles.buttonText}>Cancelar</Text>
+                        </TouchableOpacity> 
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -43,31 +68,45 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "rgba(0,0,0,0.4)", // Isso adicionará um fundo escuro transparente
+        backgroundColor: "rgba(0,0,0,0.4)", 
     },
 
     modalView: {
         width: '80%',
-        backgroundColor: "white",
+        backgroundColor: "#333333",
         borderRadius: 20,
         padding: 35,
         alignItems: "center",
         shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
         shadowOpacity: 0.25,
         shadowRadius: 4,
-        elevation: 5
+        elevation: 5,
+        borderWidth: 1,
+        borderColor: '#663399'
     },
 
     input: {
         width: '100%',
         padding: 10,
-        borderColor: '#ccc',
+        borderColor: '#663399',
         borderWidth: 1,
         marginBottom: 10,
         borderRadius: 5,
+        color: '#fff'
     },
+
+    button: {
+        backgroundColor: '#663399',
+        marginHorizontal: 10,
+        paddingHorizontal: 5,
+        paddingVertical: 5,
+        borderRadius: 20,
+    },
+
+    buttonText: {
+        color: '#D8BFD8',
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    }
 })
--- pages/userAdmin/Shedule.jsx
+++ pages/userAdmin/Shedule.jsx
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, StyleSheet, Modal, TextInput, Button } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, FlatList, TouchableOpacity, Image, Alert } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import firestore from '@react-native-firebase/firestore';
 import ModalAgenda from '../../components/ModalAgenda';
+import getUserINFOAdmin from "../../firebase/api/admin/getUserInfoAdmin";
 
 
 export default function AgendaScreen() {
     const [selectedDate, setSelectedDate] = useState(null);
-    const [events, setEvents] = useState([]);
-    const [isModalVisible, setModalVisible] = useState(false);
+    const [ events, setEvents ] = useState([]);
+    const [ isModalVisible, setModalVisible ] = useState(false);
+    const [ usuario, setUsuario ] = useState('');
+    const [ showCalendar, setShowCalendar ] = useState(true);
+
+    function handleShowCalendar(){
+        if(showCalendar){
+            setShowCalendar(false)
+        }else{
+            setShowCalendar(true)
+        }
+    }
+
+    useEffect(() => {
+        const fetchUsuario = async () => {
+            const userInfoSnapshot = await getUserINFOAdmin();
+            if (userInfoSnapshot.exists) {
+                setUsuario(userInfoSnapshot.data());
+            }
+        };
+        fetchUsuario();
+    }, []);
 
     useEffect(() => {
         if (selectedDate) {
@@ -19,47 +40,114 @@ export default function AgendaScreen() {
                 .onSnapshot(querySnapshot => {
                     const eventsForDate = [];
                     querySnapshot.forEach(documentSnapshot => {
-                        eventsForDate.push(documentSnapshot.data());
+                        eventsForDate.push({
+                            id: documentSnapshot.id,  
+                            ...documentSnapshot.data()
+                        });
                     });
                     setEvents(eventsForDate);
+                    
                 });
 
             return () => unsubscribe();
         }
     }, [selectedDate]);
 
-    const addEvent = (eventText) => {
-        if (selectedDate && eventText) {
+    const addEvent = (titulo, descricao) => {
+        if (selectedDate && titulo && descricao) {
             firestore()
                 .collection('agenda')
                 .doc(selectedDate)
                 .collection('compromisso')
-                .add({ text: eventText });
+                .add({ 
+                    titulo_compromisso: titulo,
+                    descricao_compromisso: descricao,
+                    data_compromisso: selectedDate,
+                    criado_por: usuario.nome
+                });
         }
     };
 
+    const deleteEvent = (eventId) => {
+        if (selectedDate && eventId) {
+            firestore()
+                .collection('agenda')
+                .doc(selectedDate)
+                .collection('compromisso') 
+                .doc(eventId)
+                .delete()
+                .then(() => {
+                    Alert.alert("Compromisso deletado!");
+                })
+                .catch((error) => {
+                    Alert.alert("Erro ao deletar compromisso: ", error);
+                });
+        }
+    };
+
+    function renderItem({ item }){
+        return(
+            <View style={styles.conteinerLista}>
+                <Text style={styles.textTitulo}>{item.titulo_compromisso}</Text>
+                <Text style={styles.textDescricao}>{item.descricao_compromisso}</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
+                    <Text style={styles.textInfo}>Criado por {item.criado_por} em {selectedDate}</Text>
+                    <TouchableOpacity
+                        onPress={()=> deleteEvent(item.id)}
+                    >
+                        <Image style={styles.icon} source={require('../../assets/images/deleteIcon.png')}/>
+                    </TouchableOpacity>   
+                </View>
+            </View>
+        )
+    }
+
     return (
         <SafeAreaView style={styles.conteiner}>
-            <Calendar
-                onDayPress={(day) => {
-                    setSelectedDate(day.dateString);
-                    setModalVisible(true);
-                }}
-                
-                markedDates={{
-                    [selectedDate]: {
-                        selected: true,
-                        selectedColor: '#663399',
-                    },
-                }}
-            />
+            {
+                showCalendar ?
+                <Calendar
+                    style={styles.agenda}
+                    onDayPress={(day) => {
+                        setSelectedDate(day.dateString);
+                    }}
+                    
+                    markedDates={{
+                        [selectedDate]: {
+                            selected: true,
+                            selectedColor: '#663399',
+                        },
+                    }}
+                />
+                :
+                <View></View>
+            }
 
-            {/* Sua lista de compromissos customizada */}
             <View style={styles.areaCompromissos}>
-                <Text style={{ color: 'white' }}>Compromissos para {selectedDate}</Text>
-                {events.map((event, index) => (
-                    <Text key={index} style={{ color: 'white', marginTop: 5 }}>{event.text}</Text>
-                ))}
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10 }}>
+                    <Text style={{ color: 'white' }}>Compromissos para {selectedDate}</Text>
+                    <TouchableOpacity
+                        onPress={()=> setModalVisible(true)}
+                    >
+                        <Image style={styles.icon} source={require('../../assets/images/adicionarIcon.png')}/>        
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity
+                        onPress={handleShowCalendar}
+                    >
+                        {
+                            showCalendar ?
+                            <Image style={styles.icon} source={require('../../assets/images/hideCalendar.png')}/>
+                            :
+                            <Image style={styles.icon} source={require('../../assets/images/showCalendar.png')}/>
+                        }
+                    </TouchableOpacity>
+                </View>
+                <FlatList
+                    data={events}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={renderItem}
+                />
             </View>
             <ModalAgenda isModalVisible={isModalVisible} setModalVisible={setModalVisible} addEvent={addEvent} />
         </SafeAreaView>
@@ -70,10 +158,43 @@ export default function AgendaScreen() {
 const styles = StyleSheet.create({
     conteiner: {
         flex: 1,
+        backgroundColor: '#1C1C1C'
     },
 
     areaCompromissos: {
-        flex: 1, 
-        backgroundColor: '#1C1C1C'
+        flex: 1
+    },  
+
+    icon: {
+        width: 30,
+        height: 30,
+        marginHorizontal: 10
     },
+
+    conteinerLista: {
+        backgroundColor: '#333333',
+        paddingVertical: 10,
+        paddingHorizontal: 5,
+        marginBottom: 15,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#663399',
+        marginHorizontal: 30
+    },
+
+    textTitulo: {
+        fontSize: 18,
+        color: '#fff',
+        marginLeft: 2
+    },
+
+    textDescricao: {
+        fontSize: 15,
+        color: '#fff',
+        marginLeft: 10
+    },
+
+    textInfo: {
+        color: '#fff'
+    }
 })
