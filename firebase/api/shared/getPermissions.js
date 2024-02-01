15bf48f0719e48a96fcb79a2f86d39fbbe9fb93b
Alvaro Augusto Pimentel <alvaropimentel@MacBook-Air-de-Alvaro.local>
2024-02-01 16:43:03 -0300

    fix: permissoes de galeria

--- firebase/api/shared/getPermissions.js
+++ firebase/api/shared/getPermissions.js
@@ -1,17 +1,41 @@
 import { Alert } from 'react-native';
-import { request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import { request, PERMISSIONS, RESULTS,openSettings } from 'react-native-permissions';
+
+function alertBlockedPermission() {
+    Alert.alert(
+        'Permissão bloqueada',
+        'Você bloqueou o acesso ao armazenamento externo. Para continuar, por favor, habilite a permissão nas configurações do seu dispositivo.',
+        [
+        {
+            text: 'Não Agora',
+            style: 'cancel',
+        },
+        {
+            text: 'Abrir Configurações',
+            onPress: () => openSettings(),
+        },
+        ],
+    );
+}
 
 export default async function getPermission() {
     try {
         const permissionStatus = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
 
-        if (permissionStatus === RESULTS.GRANTED) {
-            return true;
-        } else {
-            return false;
+        switch (permissionStatus) {
+            case RESULTS.GRANTED:
+                return true;
+            case RESULTS.BLOCKED:
+                return true;
+            case RESULTS.UNAVAILABLE:
+                Alert.alert('Permissão indisponível', 'Não é possível acessar o armazenamento externo.');
+                return false;
+            default:
+                return false;
         }
+
     } catch (error) {
-        Alert.alert('Erro ao solicitar permissão:', error);
+        Alert.alert('Erro ao solicitar permissão:', error.toString());
         return false;
     }
 }
